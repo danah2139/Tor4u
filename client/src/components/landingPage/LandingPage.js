ceca5ee2b4758017a3940436170086538c4347d5
danah2319 <danah2139@gmail.com>
2021-05-01 23:57:32 +0300

    refactoring

---
+++ client/src/components/landingPage/LandingPage.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { StyledContainer } from "./landingPage.style";
+import Button from "../utils/Button";
+//import { testConnection } from "../../apis/usersApi";
+
+const LandingPage = (props) => {
+  const history = useHistory();
+
+  const routeChange = (e) => {
+    props.setUserType(e.target.value);
+    history.push("/login");
+  };
+
+  useEffect(() => {
+    testConnection();
+  }, []);
+
+  return (
+    <StyledContainer className="flex-column">
+      <h1>Welcome To Tor4U</h1>
+      <p>?</p>
+      <StyledContainer>
+        <Button label="Get Service" value="Receiever" onClick={routeChange} />
+        <Button
+          label="Provide Service"
+          value="Provider"
+          onClick={routeChange}
+        />
+      </StyledContainer>
+    </StyledContainer>
+  );
+};
+export default LandingPage;
