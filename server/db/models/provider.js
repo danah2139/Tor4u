2d7ba79e3ab9d55231a1364a420704bdb9d2a7bb
danah2319 <danah2139@gmail.com>
2021-05-01 18:17:53 +0300

    refactoring

---
+++ server/db/models/provider.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const Provider = mongoose.model(Provider, {
+  name: { type: String },
+  companyName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw Error("email not valid!");
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isMobilePhone(value, ["he-IL"])) {
+          throw new Error("phone number most be a valid isrealy phone number");
+        }
+      },
+    },
+    categories: [{ name: String, price: Number }],
+    rank: { type: Number },
+    availableTimes: [{ from: Date, to: Date }],
+    address: { type: String },
+    city: { type: String },
+    region: { type: String },
+    //serviceBooked: [ ],
+  },
+});
