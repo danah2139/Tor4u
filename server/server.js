319d00a1be1c6f37b22e2c48cf5f61728dbd05d1
danah2319 <danah2139@gmail.com>
2021-05-10 20:52:58 +0300

    .

--- server/server.js
+++ server/server.js
@@ -18,11 +18,6 @@ app.use("/api", serviceBookedRouter);
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
-
-  // // Handle React routing, return all requests to React app
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
 }
 
 const PORT = process.env.PORT || 5000;
