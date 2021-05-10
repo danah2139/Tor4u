9b6226430c8b9eb0c0d22438663e66ddb430ca99
danah2319 <danah2139@gmail.com>
2021-05-10 22:35:13 +0300

    .

--- server/server.js
+++ server/server.js
@@ -14,14 +14,16 @@ app.use(express.json());
 app.use("/api", receiverRouter);
 app.use("/api", providerRouter);
 app.use("/api", serviceBookedRouter);
+const publicDirectory = path.join(__dirname, "client/build");
+app.use(express.static(publicDirectory));
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
